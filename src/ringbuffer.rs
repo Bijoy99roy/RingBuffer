8e54b62710a1f8fbd31c522715d20a06119e5fc5
Bijoy99roy <bijoy2014amp@gmail.com>
2026-04-02 02:18:38 +0530

    feat: Implement multi producer

--- src/ringbuffer.rs
+++ src/ringbuffer.rs
@@ -27,25 +27,40 @@ impl<T, const CAP: usize> RingBuffer<T, CAP> {
     }
 
     pub fn push(&self, value: T) -> Result<(), T> {
-        let head = self.head.0.load(Ordering::Relaxed);
-        let tail = self.tail.0.load(Ordering::Acquire);
+        let mut head = self.head.0.load(Ordering::Relaxed);
 
-        if head.wrapping_sub(tail) == CAP {
-            return Err(value);
+        loop {
+            let tail = self.tail.0.load(Ordering::Acquire);
+
+            if head.wrapping_sub(tail) == CAP {
+                return Err(value);
+            }
+
+            match self.head.0.compare_exchange(
+                head,
+                head.wrapping_add(1),
+                Ordering::Release,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => {
+                    // Since regular modulo operation is slow and expensive
+                    // So, instead of doing head % CAP. We are using  head & (CAP - 1)
+                    // Bitwise operation since it's faster than normal arithmatic operation
+                    // CAP needs to be power of 2 for this operation since it works only for a value that is power of 2
+                    unsafe {
+                        let buffer = &mut *self.buffer.get();
+                        buffer[head & (CAP - 1)].write(value);
+                    }
+                    return Ok(());
+                }
+                Err(current_head) => {
+                    // Since lost race to another producer, retry with new head
+                    head = current_head
+                }
+            }
+
+            self.head.0.store(head.wrapping_add(1), Ordering::Release);
         }
-
-        // Since regular modulo operation is slow and expensive
-        // So, instead of doing head % CAP. We are using  head & (CAP - 1)
-        // Bitwise operation since it's faster than normal arithmatic operation
-        // CAP needs to be power of 2 for this operation since it works only for a value that is power of 2
-        unsafe {
-            let buffer = &mut *self.buffer.get();
-            buffer[head & (CAP - 1)].write(value);
-        }
-
-        self.head.0.store(head.wrapping_add(1), Ordering::Release);
-
-        Ok(())
     }
 
     pub fn pop(&self) -> Option<T> {
